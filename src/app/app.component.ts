27452986c684a1a24d4cfc75304cef80cb024eea
clay666x <klajdi262009@live.com>
2023-01-06 00:33:23 +0100

    q

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portofolio';
+
+  showValue: boolean = true;
+  testarr = [
+    {
+      name: 'test1',
+      value: 1,
+    },
+    {
+      name: 'test2',
+      value: 2,
+    },
+    {
+      name: 'test3',
+      value: 3,
+    },
+  ];
 }
